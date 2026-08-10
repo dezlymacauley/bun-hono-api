aab89464790a2cd3708c574008c3f6c17641661f
dezlymacauley <dezlymacauley@proton.me>
2026-08-10 10:32:36 +0200

    Created initial project structure

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "none",
+  printWidth: 80
+};
+
+export default config;
