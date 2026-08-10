f3b19d58ec904b1f049743b8cd12f51fd1528e09
dezlymacauley <dezlymacauley@proton.me>
2026-08-10 13:20:47 +0200

    Wrapped code in a main function

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,32 @@
 // Imports the `Hono` class
 import { Hono } from "hono";
 
-// Connection settings
-const protocol: string = "http";
-const host: string = "127.0.0.1";
-const port: string = "4666";
-const url: string = `${protocol}://${host}:${port}`;
+// Defines the entry point of the server
+function main() {
+  // Connection settings
+  const protocol: string = "http";
+  const host: string = "127.0.0.1";
+  const port: string = "4666";
+  const url: string = `${protocol}://${host}:${port}`;
 
-// Creates a new instance of the `Hono` class
-const app = new Hono();
+  // Creates a new instance of the `Hono` class
+  const app = new Hono();
 
-// Sets up a basic route that returns text data
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+  // Sets up a basic route that returns text data
+  app.get("/", (c) => {
+    return c.text("Hello Hono!");
+  });
 
-// Starts the server
-Bun.serve({
-  port: port,
-  fetch: app.fetch
-});
+  // Starts the server
+  Bun.serve({
+    port: port,
+    fetch: app.fetch
+  });
 
-// Displays the connection settings
-console.log("\nThe server is running on:");
-console.log(`${url}\n`);
+  // Displays the connection settings
+  console.log("\nThe server is running on:");
+  console.log(`${url}\n`);
+}
+
+// Executes the `main` function
+main();
